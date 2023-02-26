a11de86fd82f25ad6ffd779d0387788306cbd24f
herily <3504863427@qq.com>
2023-02-26 14:51:39 +0800

    git commit version test

---
+++ src/main/java/com/example/demo/GitTestServlet.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet()
+public class GitTestServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("Hello,this is a git commit version test");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doGet(request, response);
+
+    }
+}
