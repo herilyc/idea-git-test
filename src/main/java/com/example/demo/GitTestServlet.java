39540c56251acfd9bd3dfb0f0e0e7c7416d1c425
herily <3504863427@qq.com>
2023-03-01 14:58:29 +0800

    demo1 branch commit version test

--- src/main/java/com/example/demo/GitTestServlet.java
+++ src/main/java/com/example/demo/GitTestServlet.java
@@ -11,6 +11,7 @@ public class GitTestServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("Hello,this is a git commit version test");
+        System.out.println("It's in demo1 branch");
     }
 
     @Override
